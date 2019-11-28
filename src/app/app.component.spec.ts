ae2edebea5e4597d550f8e899ecc2d63954ce7fd
Kim T <kimturley@gmail.com>
2019-11-27 20:24:53 -0800

    Show joined state

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'webrtc-audio-broadcast'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('webrtc-audio-broadcast');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('webrtc-audio-broadcast app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,33 @@ import { Component } from '@angular/core';
 import { RadioService } from './shared/radio.service';
 import { StationService } from './shared/station.service';
 
+declare var window: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  joined = false;
+  owner = false;
   stationId = 'test';
-  title = 'webrtc-audio-broadcast';
 
   constructor(
     public radio: RadioService
-  ) { }
+  ) {
+    window.radio = this.radio;
+  }
 
   async create() {
     const station = new StationService(this.radio.context, this.radio.outgoing);
     await station.start();
+    this.owner = true;
     this.join();
   }
 
   join() {
+    this.joined = true;
     this.radio.leaveAll();
     this.radio.join(this.stationId);
   }
